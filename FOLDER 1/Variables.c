778c26a9fb1c301cb6ca7fe2c8bcccadc75c3274
MJ1600 <113929710+MJ1600@users.noreply.github.com>
2022-10-03 20:03:17 +0530

    Create Variables.c

---
+++ FOLDER 1/Variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   // Creating variables
+   
+int myNum = 5;
+float myFloatNum = 5.978;
+char myLetter = 'K';
+
+  // Print Variables
+  
+printf("%d\n", myNum);
+printf("%f\n", myFloatNum);
+printf("%c\n", myLetter);
+
+return 0;
+}
