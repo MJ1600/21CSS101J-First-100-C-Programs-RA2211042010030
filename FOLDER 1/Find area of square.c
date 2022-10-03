ad72224353601e2fa0b5d72623b441f55a92e37a
MJ1600 <113929710+MJ1600@users.noreply.github.com>
2022-10-03 19:35:37 +0530

    Create Find area of square.c

---
+++ FOLDER 1/Find area of square.c
@@ -0,0 +1,14 @@
+//Find the area of square
+
+#include <stdio.h>
+int main()
+{
+  int s;
+  printf("Enter the value of side");
+  scanf("%d",&s);
+  
+  int area;
+  area  = s * s;
+  printf("Area of square = %d", area);
+  return 0;
+}
