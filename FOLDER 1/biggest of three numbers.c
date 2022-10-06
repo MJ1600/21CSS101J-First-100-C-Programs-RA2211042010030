d7d24c2559a88d7d374dd9febcc68dcbeab2231e
MJ1600 <113929710+MJ1600@users.noreply.github.com>
2022-10-06 18:56:03 +0530

    Create biggest of three numbers.c

---
+++ FOLDER 1/biggest of three numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{ 
+   int a,b,c;
+   printf("Enter number a\n");
+   scanf("%d",&a);
+   printf("Enter number b\n");
+   scanf("%d", &b);
+   printf("Enter number c\n");
+   scanf("%d", &c);
+   if(a>b && a>c);
+  { printf("a is the largest: ", a);}
+   if(b>c && b>a);
+  { printf("b is the largest: ", b);}
+   if(c>a && c>b);
+  { printf("c is the largest: ", c);}
+  return 0;
+}
