6dfa74ddc9f88f99a2073a40f85875e57a7cdb3c
MJ1600 <113929710+MJ1600@users.noreply.github.com>
2022-10-07 16:54:15 +0530

    Create stars.c

---
+++ FOLDER 1/stars.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+*
+* *
+* * *
+* * * *
+* * * * *
+void main()
+{
+  int x, i, j;
+  printf("How many lines stars (*) should be printed? :");
+  scanf("%d", &x);
+  for(i=1;i<=x;i++)
+  { 
+     for(j=1;j<=i;j++)
+     {   
+      printf("*");
+      }
+      printf("\n");
+      }
+ }
