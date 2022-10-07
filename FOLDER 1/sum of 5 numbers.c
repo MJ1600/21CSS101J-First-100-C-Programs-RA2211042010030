e1b69a8e2e9028e407cd23491b7e990c601d2ecf
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-07 21:24:18 +0530

    Create sum of 5 numbers.c

---
+++ FOLDER 1/sum of 5 numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int p,q,r,s,t;
+    
+    printf("Enter the numbers:");
+    scanf("%d" ,&p,&q,&r,&s,&t);
+    
+    sum=p+q+r+s+t;
+    printf("Sum of the digits :");
+    printf("%d",sum);
+    return 0;
+}
