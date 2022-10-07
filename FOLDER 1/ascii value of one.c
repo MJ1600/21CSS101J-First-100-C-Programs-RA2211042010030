2fb8fa2f9961a5431d151723a8d9a83bbba8a11b
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-07 23:17:33 +0530

    Create ascii value of one.c

---
+++ FOLDER 1/ascii value of one.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  int binary three = 0611;
+  printf("binary tree = %d\n", binary tree);
+  
+  int octaleight=010;
+  printf("octaleight value = %d\n", octaleight);
+  
+  int hexten = 0 * A;
+  printf("hexten value = %d\n", hexten);
+  
+  int ascii value of one = '1';
+  printf("ascii value of one value = %d\n", ascii value of value);
+  
+  int ascii value of A = 'A';
+  printf("ascii value of A value = %d\n", ascii value of A);
+  }
