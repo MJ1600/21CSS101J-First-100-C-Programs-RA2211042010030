a732e09bee3b949f305ab983706ccefa3db38a95
MJ1600 <113929710+MJ1600@users.noreply.github.com>
2022-10-07 15:24:01 +0530

    Create declare variables.c

---
+++ FOLDER 1/declare variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+  int Num =16;
+  int WEIGHT = 50;
+  float HEIGHT = 5.6;
+  char NAME = "Mimansha";
+  
+  printf("Num : %d \n WEIGHT : %d \n HEIGHT : %g \n NAME = %s \n", Num, WEIGHT, HEIGHT, NAME);
+}
