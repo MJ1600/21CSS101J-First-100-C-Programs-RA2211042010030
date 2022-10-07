3beebca0b4667147aedf6f7a2e7337a2ac56acd6
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-07 23:23:10 +0530

    Create signed and unsigned value.c

---
+++ FOLDER 1/signed and unsigned value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+ signed int number1 = -20, number2 = 20;
+ unsigned int number3 = -1, number4 = 1;
+ 
+ printf("Given signed values are %d and %d\n", number1, number2);
+ 
+ printf("Given unsigned values are %d and %d\n", number3, number4);
+ }
