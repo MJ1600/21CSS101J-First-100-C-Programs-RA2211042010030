942899b93f54b42a488ad2e1fa5083a2a7992721
MJ1600 <113929710+MJ1600@users.noreply.github.com>
2022-10-07 14:16:49 +0530

    Create area of triangle.c

---
+++ FOLDER 1/area of triangle.c
@@ -0,0 +1,10 @@
+/* To find the area of triangle */
+#include <stdio.h>
+int main()
+{
+  float  area, base, height;
+  printf("Enter base, height \n");
+  scanf("%f  %f, &base, &height);
+  area = 0.5 * base * height;
+  printf("Area =  %f  \n", area);
+  }
