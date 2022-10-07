8a3caac4fbe1c09d25b637cc587c10ab663a6c6b
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-07 21:42:34 +0530

    Create to get student name, RN, mark.c

---
+++ FOLDER 1/to get student name, RN, mark.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ int register no, mark, average;
+ char Name;
+ 
+ printf("Name = %c\n", Name);
+ scanf("%s", &Name);
+ 
+ printf("Register no: %d\n", register no);
+ scanf("%d", &register no);
+ 
+ printf("Mark=%d\n", mark);
+ scanf("%d", &mark);
+ 
+ printf("Average=%d\n", (Mark1 + Mark2 + Mark3)/3);
+ }
