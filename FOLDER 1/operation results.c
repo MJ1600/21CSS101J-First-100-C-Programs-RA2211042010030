1503834e4364a0bfcb44e279893b98244c8037a1
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-08 14:29:35 +0530

    Create operation results.c

---
+++ FOLDER 1/operation results.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int num1=100, num2=66;
+  
+  printf("Addition Result = %d\n", (num1+num2) );
+  printf("Subtraction Result = %d\n", (num1-num2) );
+  printf("Multiplication Result = %d\n", (num1*num2) );
+  printf("Division Result = %f\n", ( (float)num1/num2 ) );
+  printf("Remainder = %d\n", (num1%num2) );
+  }
