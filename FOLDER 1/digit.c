924005eff94d280f30df7f8ffb5b3cc213e31762
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-09 01:34:46 +0530

    Create digit.c

---
+++ FOLDER 1/digit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int n, last digit;
+  printf("enter digit: ");
+  scanf("%d", &n);
+  last digit=n%10;
+  printf("last digit=%d\n, last digit);
+  return 0;
+  }
