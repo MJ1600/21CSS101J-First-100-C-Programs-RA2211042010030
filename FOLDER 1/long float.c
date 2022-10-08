a5513129dc3fba0c98f9a5e3862025df109ca5fb
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-08 15:24:41 +0530

    Create long float.c

---
+++ FOLDER 1/long float.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+  float num1=12.6, num2=13.7;
+  printf("Result of addition = %lf\n", (num1+num2) );
+  printf("Result of subtraction = %lf\n", (num1-num2) );
+  printf("Result of multiplication = %lf\n", (num1*num2) );
+  printf("Result of division = %lf\n", (num1/num2) );
+  }
