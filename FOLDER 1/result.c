8b26cadbebfa6f85673869a510584cf26a56bbff
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-08 16:12:58 +0530

    Create result.c

---
+++ FOLDER 1/result.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+   int num1=8;
+   float num2=6.9;
+   char ch='s';
+   
+   printf("Result1=%d\n", (num1>5) );
+   printf("Result2=%d\n", (num1+num2)<=b);
+   printf("Result3=%d\n", (ch==126) );
+   printf("Result4=%d\n", (ch1='p') );
+   printf("Result5=%d\n", (ch>=10*(num1+num2)) );
+   }
