0ee12dccc6fba9700c8233600d1d3e8e7bfde80a
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-09 00:37:21 +0530

    Create congratulations.c

---
+++ FOLDER 1/congratulations.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int marks;
+  printf("\n Enter marks from 0-100 : ");
+  scanf("%d", &marks);
+  
+  if(marks < 16);
+   {
+     printf("\n Sorry..! You have failed!");
+     }
+     else
+     {
+      printf("\n congratulations...! You have passed!");
+      }
+      return 0;
+      }
