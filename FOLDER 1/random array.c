b407d55690b6c9a37f47e5b335d152fb9ae9bc2f
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-09 22:09:47 +0530

    Create random array.c

---
+++ FOLDER 1/random array.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+   int array[100];
+   
+   printf( "The value of \"array\" is %x\n", array );
+   printf( "The value of \"&array[0]\" is %x\n", 
+         &array[0]  );
+ }
