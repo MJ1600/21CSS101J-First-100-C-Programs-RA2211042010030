b88e2b6198bbe27a5fd39d709a1035d501cf9796
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-09 15:35:01 +0530

    Create decimal, octal, hexadecimal.c

---
+++ FOLDER 1/decimal, octal, hexadecimal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+ int n;
+ printf("Enter a value:\n ");
+ scanf("%d", &n);
+ printf("Decimal value = %d\n", n);
+ printf("Octal value = %O\n", n);
+ printf("Hexadecimal value = %x\n", n);
+ }
