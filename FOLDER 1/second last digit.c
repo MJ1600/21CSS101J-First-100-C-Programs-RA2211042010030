179b70a63c68e078418d3065b9e9c7d51f55abfb
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-09 15:08:11 +0530

    Create second last digit.c

---
+++ FOLDER 1/second last digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int second-last digit (int N)
+{
+  N=N/10;
+  return N%10;
+  }
+  int main()
+  {
+    int N, last digit, multiply;
+    printf("Enter a number: ");
+    scanf("%d", &N);
+    last digit=N%10;
+    printf("last digit: %d/n", last digit);
+    printf("second-last digit: %d\n", second-last digit(N) );
+    multiply = last digit*second-last digit(N);
+    printf("Multiplication of last digit and second-last digit: %d", multiply);
+    }
