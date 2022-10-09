c6c987882871d5971f711109e15cb594b0bf8a2e
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-09 18:03:20 +0530

    Create decrement.c

---
+++ FOLDER 1/decrement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int x, y;
+  
+  x = 5;
+  y = 7;
+  
+  printf("The value of --x is %d\n", --x);
+  printf("The value of y-- is %d\n", y--);
+  
+  printf("After the decrement x is %d\n", x);
+  printf("After the decrement y is %d\n", y);
+  }
