ff432f2603b2914edba331d4660c76f78fb9dee3
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-09 21:58:25 +0530

    Create fpoint.c

---
+++ FOLDER 1/fpoint.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    float fpoint;
+    double lfpoint;
+    
+    fpoint = 123.45;
+    printf( "%f\n", fpoint );
+    fpoint /= 3.30;
+    fpoint *= 3.30;
+    printf( "%f\n", fpoint );
+    
+    lfpoint = 123.45;
+    printf( "%f\n", lfpoint );
+    lfpoint /= 3.30;
+    lfpoint *= 3.30;
+    printf( "%f\n", lfpoint );
+    }
+    
+    
