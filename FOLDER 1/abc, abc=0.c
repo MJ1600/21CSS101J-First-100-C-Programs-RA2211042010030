08cf7bb2f91c8682644c5cbb3bb3278bc58b2055
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-09 21:45:36 +0530

    Create abc, abc=0.c

---
+++ FOLDER 1/abc, abc=0.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   if(strcmp( "abc", "abc" ) ==0 )
+   {
+       printf("The two are the same\n" );
+   }
+   else
+   {
+       printf("The two are NOT the same\n" );
+   }
+}
