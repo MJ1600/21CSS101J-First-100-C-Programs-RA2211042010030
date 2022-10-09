c8cdc1a5221ed65b8c11b6c9aafff5211722e464
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-09 17:50:20 +0530

    Create increment.c

---
+++ FOLDER 1/increment.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/* This program illustrates the increment operator */
+int main()
+{
+  int x, y;
+  
+  x = 6;
+  y = 6;
+   
+  printf("The value of ++x is %d\n", ++x );
+  printf("The value of y++ is %d\n", y++ );
+  
+  printf("After the increment x is %d\n", x);
+  printf("After the increment y is %d\n", y );
+  }
