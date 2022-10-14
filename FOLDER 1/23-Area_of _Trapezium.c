3b9fb70833db5fe2e20e6086b867b0edccf32e36
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-15 02:06:37 +0530

    Update and rename area of trapezium.c to 23-Area_of _Trapezium.c

--- FOLDER 1/23-Area_of _Trapezium.c
+++ FOLDER 1/23-Area_of _Trapezium.c
@@ -2,6 +2,9 @@
 int main()
 {
   float side1, side2, height, area;
+  
+   printf("Register Number: RA2211042010030");
+
   printf("Enter the height and parallel sides of the trapezium respectively \n");
   scanf("%f %f %f", &height, &side1, &side2);
   area = 0.5*(side1+side2)*height;
