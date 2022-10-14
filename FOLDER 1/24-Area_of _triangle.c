263df2bfda26a0793c305776a302d8bf1848d322
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-15 02:07:19 +0530

    Update and rename area of triangle.c to 24-Area_of _triangle.c

--- FOLDER 1/24-Area_of _triangle.c
+++ FOLDER 1/24-Area_of _triangle.c
@@ -3,6 +3,9 @@
 int main()
 {
   float  area, base, height;
+  
+   printf("Register Number: RA2211042010030");
+
   printf("Enter base, height \n");
   scanf("%f  %f, &base, &height);
   area = 0.5 * base * height;
