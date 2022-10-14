b8ed2346936e48b1be5006263cdd8d74c085028f
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-15 02:51:56 +0530

    Update and rename sum of integer numbers.c to 85-sum of integer numbers.c

--- FOLDER 1/85-sum of integer numbers.c
+++ FOLDER 1/85-sum of integer numbers.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main()
 {
+   printf("Register Number: RA2211042010030");
+
   printf("Sum of integer numbers = %d\n", 10+20);
   printf("Sum of float numbers = %f\n", 10.56+78.99);
   }
