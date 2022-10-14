c414bb48977654ec711468db4d035e3223dd9a55
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-15 02:39:21 +0530

    Update and rename perimeter of circle upto 2dp.c to 64-perimeter of circle upto 2dp.c

--- FOLDER 1/64-perimeter of circle upto 2dp.c
+++ FOLDER 1/64-perimeter of circle upto 2dp.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 void main()
 {
+    printf("Register Number: RA2211042010030");
+
    float PI=3.14, radius, perimeter;
   printf("Enter radius of the circle: ");
   scanf("%f", &radius);
