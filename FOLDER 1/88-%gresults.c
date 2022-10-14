53eb10b07d759414e51965c66e7488b732b7ddc5
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-15 02:53:33 +0530

    Update and rename results.c to 88-%gresults.c

--- FOLDER 1/88-%gresults.c
+++ FOLDER 1/88-%gresults.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 void main()
 {
+   printf("Register Number: RA2211042010030");
+
   printf("Result1 = %g\n", (2+3.9) );
   printf("Result2 = %i\n", ('A'+(0)) );
   printf("Result3 = %lf\n", (6.9+7.32) );
