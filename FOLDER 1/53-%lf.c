7eab429e3e2dc2d793d8dc7ba34a8533bbc05a5d
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-10-15 02:32:54 +0530

    Update and rename long float.c to 53-%lf.c

--- FOLDER 1/53-%lf.c
+++ FOLDER 1/53-%lf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 void main()
 {
+   printf("Register Number: RA2211042010030");
+
   float num1=12.6, num2=13.7;
   printf("Result of addition = %lf\n", (num1+num2) );
   printf("Result of subtraction = %lf\n", (num1-num2) );
